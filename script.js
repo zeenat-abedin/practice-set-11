2175951de92476e177fc413915ccbf4cae4416f4
zeenat <zeenu.23@gmail.com>
2024-03-22 13:43:21 +0530

    "Added getters and setters for complex number class properties; added Human and Student classes with walk method"

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 
 //5. Use getters and setters to set and get the real and imaginary parts of the complex number
 
+//1, 2, and 5 soln.
 class Complex{
     constructor(real, imaginary) {
         this.real = real
@@ -17,10 +18,47 @@ class Complex{
         this.real = this.real + num.real
         this.imaginary =  this.imaginary + num.imaginary
     }
+    get real() {
+      return this._real
+    }
+    set real(newReal) {
+        this._real = newReal
+    }
+    get imaginary(){
+       return this._imaginary    
+    }
+    set imaginary(newImaginary) {
+        this._imaginary = newImaginary
+    }
 }
 
 let a = new Complex(2, 4)
-let b = new Complex(1, 6)
+console.log(a.real, a.imaginary); 
 
+a.real = 10
+a.imaginary = 10
+console.log(a.real, a.imaginary); 
+
+let b = new Complex(6,2)
 a.add(b)
-console.log(`${a.real}, ${a.imaginary}`); 
+console.log(`${a.real}+${a.imaginary}i`); 
+
+//3. and 4. soln.
+class Human{
+    constructor(name, favfood) {
+        this.name = name
+        this.favfood = favfood
+    }
+    walk() {
+        console.log(this.name + "Human is walking")
+    }
+}
+
+class Student extends Human{
+    walk() {
+        console.log(this.name + " " + "Student is walking")
+    }
+}
+
+let o = new Student("John", "Pizza");
+o.walk()
